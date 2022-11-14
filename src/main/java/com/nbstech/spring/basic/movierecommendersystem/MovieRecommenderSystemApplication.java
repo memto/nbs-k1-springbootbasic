d4a740977fbd5746392f46c16b123ba609fec72c
memto <dvn0zzz@gmail.com>
2022-11-15 06:44:23 +0700

    2.2: Tigh Coupling (https://www.educative.io/courses/guide-spring-5-spring-boot-2/R8pJ4nYWLOK)

--- src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,13 +1,25 @@
 package com.nbstech.spring.basic.movierecommendersystem;
 
+import com.nbstech.spring.basic.movierecommendersystem.Lesson1.MovieRecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+		//SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+		//create object of RecommenderImplementation class
+		MovieRecommender recommender = new MovieRecommender();
+
+		//call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+		//display results
+		System.out.println(Arrays.toString(result));
 	}
 
 }
