63b2daedb8fd336d2da51b9a2b12bf8df1d98861
memto <dvn0zzz@gmail.com>
2022-11-16 13:59:34 +0700

    3.7: Config Spring App manually (https://www.educative.io/courses/guide-spring-5-spring-boot-2/39WvjGDqwz9)

--- src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -3,20 +3,28 @@ package com.nbstech.spring.basic.movierecommendersystem;
 import com.nbstech.spring.basic.movierecommendersystem.Lesson3.CollaborativeFilter;
 import com.nbstech.spring.basic.movierecommendersystem.Lesson3.MovieRecommender;
 import com.nbstech.spring.basic.movierecommendersystem.Lesson3.ContentBasedFilter;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 
-		//ApplicationContext manages the beans and dependencies
-		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+		// ApplicationContext manages the beans and dependencies
+//		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+		AnnotationConfigApplicationContext appContext =
+				new AnnotationConfigApplicationContext(MovieRecommenderSystemApplication.class);
 
 		//use ApplicationContext to find which filter is being used
 		MovieRecommender recommender = appContext.getBean(MovieRecommender.class);
@@ -26,5 +34,7 @@ public class MovieRecommenderSystemApplication {
 
 		//display results
 		System.out.println(Arrays.toString(result));
+
+		appContext.close();
 	}
 }
